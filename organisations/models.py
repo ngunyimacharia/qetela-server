81a9a735b21e372b19fa01fb7accb5a1e61437d1
Kelvin Macharia <ngunyimacharia@gmail.com>
2019-02-16 00:20:22 +0000

    Mongodb integrated CRUD Organisation

--- organisations/models.py
+++ organisations/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 #model for organisations
 class Organisation(models.Model):
     name = models.CharField(max_length=200) #string field max 200
-    website = models.CharField(max_length=2083) #string field max 2083. That's maximum for a website string
+    website = models.CharField(max_length=2083,blank=True,null=True) #string field max 2083. That's maximum for a website string
     branches = models.BooleanField(default=0) #whether the organisation has branches. Default is no branches
     cf_frequency = models.IntegerField(default=7) #frequency of getting feedback from employees on KPIs
     created = models.DateTimeField(auto_now_add=True) #date record was created
--- qetela/settings.py
+++ qetela/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'qetela.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'qetela',
     }
 }
 
