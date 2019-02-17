8e7000b4777ca345d5f246397eb05dbf7857afcf
Kelvin Macharia <ngunyimacharia@gmail.com>
2019-02-17 05:14:27 +0000

    Bug fix

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,6 +4,6 @@ cd /root/qetela-server
 git pull
 source /root/env/bin/activate
 pip install -r requirements.txt
-./manage.py migrate
+python manage.py migrate
 python manage.py collectstatic --noinput
 sudo supervisorctl restart qetela
