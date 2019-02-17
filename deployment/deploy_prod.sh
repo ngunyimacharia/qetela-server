a8f54505d09731b3dc71eacabde1d4d4b4ee77b3
Kelvin Macharia <ngunyimacharia@gmail.com>
2019-02-17 04:06:23 +0000

    Updates to deploy script

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cd ~/qetela-server
+cd /root/qetela-server
 git pull
 source /root/env/bin/activate
 pip install -r requirements.txt
 ./manage.py migrate
+python manage.py collectstatic --noinput
 sudo supervisorctl restart qetela
