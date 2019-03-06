f77f436e901481ed0a0490d6f7cf038bfda78be3
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2019-03-06 14:03:16 +0000

    Onboarding model and migrations

---
+++ onboarding/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+#model for onboarding kits
+class Kit(models.Model):
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+#model for onboarding kits tasks
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+#model for onboarding sesssions
+class Session(models.Model):
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    kit = models.ForeignKey(
+        'onboarding.Kit',
+        on_delete=models.CASCADE,
+    )
+    buddy = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='session_buddy_user',
+    )
+    completed = models.BooleanField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+#model for onboarding session progress
+class Progress(models.Model):
+    session = models.ForeignKey(
+        'onboarding.Session',
+        on_delete=models.CASCADE,
+    )
+    task = models.ForeignKey(
+        'onboarding.Task',
+        on_delete=models.CASCADE,
+    )
+    completed = models.BooleanField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- qetela/settings/base.py
+++ qetela/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'organisations',
     'goals',
     'chats',
+    'onboarding',
 ]
 
 MIDDLEWARE = [
